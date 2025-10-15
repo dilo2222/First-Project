93ebadb7a09a1b3bc5bb6ed54becb4d566356ee8
richycala33 <201261859+dilo2222@users.noreply.github.com>
2025-10-15 14:55:46 +0500

    added some functionality far away from being perfect

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const listEl = document.querySelector(".important__list")
 const wrapperEl = document.querySelector(".important__wrapper")
+const overlayEl = document.querySelector(".overlay")
+const popEl = document.querySelector(".popup")
+const addEl = document.querySelector(".popup_add")
+const cancelEl = document.querySelector(".popup_remove")
+const inputEl = document.querySelector(".popup__input")
 
 
-const createButton = (btnName, parentEl) => {
-    const btn = document.createElement("button")
-    btn.textContent = btnName
-    parentEl.append(btn)
-}
+const addBtn = document.querySelector(".important__add")
+addBtn.addEventListener("click", function (e) {
+    overlayEl.classList.toggle("hidden")
+    popEl.classList.toggle("hidden")
+})
 
-//const addBTn = createButton("Add", wrapperEl )
-//const removeBTn = createButton("Remove", wrapperEl )
+cancelEl.addEventListener("click", function (e) {
+    overlayEl.classList.toggle("hidden")
+    popEl.classList.toggle("hidden")
+})
+
+addEl.addEventListener("click", function (e) {
+    if (inputEl.value) {
+        const liEl = document.createElement("li")
+        liEl.classList.add("important__item")
+        liEl.textContent = inputEl.value
+        listEl.append(liEl)
+        overlayEl.classList.toggle("hidden")
+        popEl.classList.toggle("hidden")
+        inputEl.value = ""
+    }
+})
